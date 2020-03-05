c9e7bb4ed8d6574a28664469a2c5408d43e13e4a
Maksim Kirilko <maksim.kirilko@skywindgroup.com>
2020-03-05 16:26:20 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const open = bracketsConfig.map(([bracket]) => bracket);
+  const closed = bracketsConfig.map(([, bracket]) => bracket);
+
+  for (let i = 0; i < str.length; i++) {
+      const symbol = str[i];
+      const lastInStack = stack[stack.length - 1];
+      if (open.includes(symbol)) {
+          if (closed.includes(symbol)) {
+              symbol === lastInStack ? stack.pop() : stack.push(symbol);
+              continue
+          }
+          stack.push(symbol);
+          continue
+      }
+      if (closed.includes(symbol)) {
+          const closeIndex = closed.indexOf(symbol);
+          open.indexOf(lastInStack) === closeIndex ? stack.pop() : stack.push(symbol)
+      }
+
+  }
+  return stack.length === 0;
+};
